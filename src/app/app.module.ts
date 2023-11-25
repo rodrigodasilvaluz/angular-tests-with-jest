346e8cc0def372229dab308f66f425c21b987b8d
dextrarsilva <dextra.rsilva@creditas.com>
2023-11-24 23:36:07 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule, BrowserAnimationsModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
